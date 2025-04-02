5556b111dc4ca3bd3d34f077b84d7d09188e70a3
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-02 15:33:13 +0100

    Add Spring Boot boilerplate

---
+++ irsys/src/main/java/com/ir/irsys/IrsysApplication.java
@@ -0,0 +1,13 @@
+package com.ir.irsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrsysApplication.class, args);
+    }
+
+}
