9441693420b5f317104b1e16b6ae12fd06d9e338
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-02 16:12:00 +0100

    Add Constructor to Ranker class

--- irsys/src/main/java/com/ir/irsys/application/Ranker.java
+++ irsys/src/main/java/com/ir/irsys/application/Ranker.java
@@ -1,4 +1,31 @@
 package com.ir.irsys.application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Ranker {
+
+    private BagOfWords bow;
+    private List<Map<String, Integer>> documentVectors;
+    private List<String> documentIds;
+    private List<String> documentTexts;
+
+    public Ranker(BagOfWords bow, List<String> documentIds, List<String> documentTexts) {
+        this.bow = bow;
+        this.documentIds = documentIds;
+        this.documentTexts = documentTexts;
+        this.documentVectors = new ArrayList<>();
+
+        for (String text : documentTexts) {
+            List<String> tokens = bow.preprocessText(text);
+            Map<String, Integer> vector = bow.vectorizeDocument(tokens);
+            documentVectors.add(vector);
+        }
+    }
+
+
+
+
+
 }
