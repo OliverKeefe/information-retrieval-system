1cdd7edc90e2884c81a0e588c3b2f418ba953292
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-04 15:10:53 +0100

    Add methods to calculate eval scores

--- irsys/src/main/java/com/ir/irsys/application/Evaluator.java
+++ irsys/src/main/java/com/ir/irsys/application/Evaluator.java
@@ -1,4 +1,31 @@
 package com.ir.irsys.application;
 
+import java.util.List;
+import java.util.Set;
+
 public class Evaluator {
+
+    public static double calculatePrecision(List<String> retrievedDocIds, Set<String> relevantDocIds, int k) {
+        int retrievedSize = Math.min(k, retrievedDocIds.size());
+        int relevantCount = 0;
+
+        for (int i = 0; i < retrievedSize; i++) {
+            if (relevantDocIds.contains(retrievedDocIds.get(i))) {
+                relevantCount++;
+            }
+        }
+        return (double) relevantCount / retrievedSize;
+    }
+
+    public static double calculateRecall(List<String> retrievedDocIds, Set<String> relevantDocIds, int k) {
+        int retrievedSize = Math.min(k, retrievedDocIds.size());
+        int relevantCount = 0;
+
+        for (int i = 0; i < retrievedSize; i++) {
+            if (relevantDocIds.contains(retrievedDocIds.get(i))) {
+                relevantCount++;
+            }
+        }
+        return (double) relevantCount / relevantDocIds.size();
+    }
 }
