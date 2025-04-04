ea30c6e07eee0f6b8343345697ae2c24a61d76f5
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-04 15:10:11 +0100

    Add Beans

--- irsys/src/main/java/com/ir/irsys/config/IRConfig.java
+++ irsys/src/main/java/com/ir/irsys/config/IRConfig.java
@@ -1,4 +1,32 @@
 package com.ir.irsys.config;
 
+import com.ir.irsys.application.BagOfWords;
+import com.ir.irsys.application.Ranker;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
 public class IRConfig {
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create("mongodb://admin:password@localhost:27017/?authSource=admin");
+    }
+
+    @Bean
+    public BagOfWords bagOfWords(MongoClient mongoClient) {
+        return new BagOfWords(mongoClient, "cranfield_db", "documents");
+    }
+
+    @Bean
+    public Ranker ranker(BagOfWords bow) {
+        List<String> docIds = List.of("200", "230", "247", "250", "289");
+        List<String> texts = bow.readDocuments();
+
+        return new Ranker(bow, docIds, texts);
+    }
 }
